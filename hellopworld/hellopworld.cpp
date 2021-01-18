990837fb08c30af66651de1aa637a1e5cbbb44d4
kripa59 <kripa59@yandex.ru>
2021-01-18 22:05:17 +0300

    homework_1

---
+++ hellopworld/hellopworld.cpp
@@ -0,0 +1,36 @@
+﻿// hellopworld.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+
+// Следующая функция печатает
+void print() 
+{
+	// Вызываем оператор << на объекте типо std::cout
+	std::cout << "Hello man!/n";
+}
+
+/*
+ * 1
+ * 2
+ */
+int main()
+{	
+	int x = 100;
+	3 + 7;
+	int y = x + 100;
+
+	int b = 0;
+	b = b + 2;
+
+	int test;
+	test = 100500;
+
+	int test2 = 1005001;
+
+
+	int mult = x * y;
+	int random = 1002;
+
+    std::cout << random;
+}
